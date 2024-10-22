59bb06126730c05832d94863f9dd173b4a43bbd9
ItsRitik <rk10069n@pace.edu>
2024-10-22 18:24:30 -0400

    Created Header

--- week6-explore-bootstrap/src/App.js
+++ week6-explore-bootstrap/src/App.js
@@ -1,34 +1,39 @@
 // App.js
 import React from "react";
 import Sidebar from "./components/sidebar";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 // import SummaryCards from "./components/SummaryCards";
 // import ChartWidget from "./components/ChartWidget";
 // import RecentActivity from "./components/RecentActivity";
 // import PerformanceMetrics from "./components/PerformanceMetrics";
 // import Footer from "./components/Footer";
 // import { Container, Row, Col } from "react-bootstrap";
+import "./App.css"; // Import global styles
 
-const App = () => (
-  <div className="d-flex">
-    <Sidebar />
-    {/* <div className="flex-grow-1">
+const App = () => {
+  return (
+    <div className="app-layout">
       <Header />
-      <Container fluid className="mt-4">
-        <SummaryCards />
-        <Row>
-          <Col md={8}>
-            <ChartWidget />
-          </Col>
-          <Col md={4}>
-            <PerformanceMetrics />
-          </Col>
-        </Row>
-        <RecentActivity />
-      </Container>
-      <Footer /> 
-    </div>*/}
-  </div>
-);
+      <div className=" d-flex">
+        <Sidebar />
+        {/* <div className="content-container">
+          <Container fluid className="mt-4">
+            <SummaryCards />
+            <Row>
+              <Col md={8}>
+                <ChartWidget />
+              </Col>
+              <Col md={4}>
+                <PerformanceMetrics />
+              </Col>
+            </Row>
+            <RecentActivity />
+          </Container>
+          <Footer />
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default App;
